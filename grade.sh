320552262180bb4f4f824fc8da3a3e3a0880f471
Jayden Xia <goats@Jaydens-Macbook.local>
2024-05-28 10:03:15 -0700

    new changes

--- grade.sh
+++ grade.sh
@@ -1,32 +1,51 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+
 rm -rf student-submission
 rm -rf grading-area
 
+
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
 
-if [[ ! -f student-submission/ListExamples.java ]]; then
-    echo "ListExamples not found"
-    echo "Grade: 0"
-    exit 1
+git clone $1 student-submission
+if [ $? -ne 0 ]; then
+echo "Failed to clone repository"
+echo "Grade: 0"
+exit 1
+fi
+echo 'Finished cloning'
+if [[ -f student-submission/ListExamples.java ]]; then
+echo "ListExamples.java found"
 else
-    echo "File Existed"
+echo "ListExamples.java does not exist"
+echo "Grade: 0"
+exit 1
 fi
 
-cp student-submission/ListExamples.java TestListExamples.java grading-area
-cp -r lib grading-area
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
+
 
 cd grading-area
 javac -cp $CPATH ListExamples.java TestListExamples.java
+if [ $? -ne 0 ]; then
+echo "Compilation failed"
+echo "Grade: 0"
+exit 1
+fi
 
-echo "This is the exit code of javac (previous command): $?."
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+TEST_RESULT=$?
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+
+if [ $TEST_RESULT -eq 0 ]; then
+echo "All tests passed"
+echo "Grade: 100"
+else
+echo "Some tests failed"
+echo "Grade: 50"
+fi
