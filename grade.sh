285ec224a0e4c5a936555b32c6585b839afd18d7
Jayden Xia <goats@Jaydens-Macbook.local>
2024-05-28 09:54:15 -0700

    New Changes

--- grade.sh
+++ grade.sh
@@ -8,7 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ ! -f student-submission/ListExamples.java ]]; then
+    echo "ListExamples not found"
+    echo "Grade: 0"
+    exit 1
+else
+    echo "File Existed"
+fi
 
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH ListExamples.java TestListExamples.java
+
+echo "This is the exit code of javac (previous command): $?."
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
